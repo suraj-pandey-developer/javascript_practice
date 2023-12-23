79d8a0275e8b1a227101582776e81d5f659ec3a3
suraj-pandey-developer <officialsurajpandey55@gmail.com>
2023-12-23 21:11:41 +0530

    add : fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,17 @@
+console.log("server is runnning")
+console.log(this)
+
+function maxi() {
+    return 1+2 ;
+}
+
+var v = 10 ;
+console.log(v);
+console.log(maxi);
+console.log(maxi());
+
+// ---------------- function ------------------
+
+// conventional function 
+
+// annoynomys function
