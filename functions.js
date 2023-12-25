944845de7a60c924b302530fe39546c382e7afb6
suraj-pandey-developer <officialsurajpandey55@gmail.com>
2023-12-25 15:20:16 +0530

    equality operator

---
+++ functions.js
@@ -0,0 +1,16 @@
+// ---------------- function ------------------
+
+// conventional function 
+
+
+function greet() {
+    console.log("hello world");
+}
+// annoynomys function
+// function which does not have name but we assigned it
+let greet1 = function() {
+    console.log("hello annoynomus")
+}
+
+greet();
+greet1();
